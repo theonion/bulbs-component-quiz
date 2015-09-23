0f96da89506474c98e7f667e1bb51f705755c119
Andrew Kos <akos@theonion.com>
2015-09-23 17:14:10 -0500

    Quiz answer cleanup

--- src/bulbs-cms/quiz-edit/quiz-edit-questions/quiz-edit-questions-question/quiz-edit-questions-question-answer/quiz-edit-questions-question-answer.js
+++ src/bulbs-cms/quiz-edit/quiz-edit-questions/quiz-edit-questions-question/quiz-edit-questions-question-answer/quiz-edit-questions-question-answer.js
@@ -1,15 +1,17 @@
 'use strict';
 
-angular.module('bulbs.quiz.questions.question.answer', [])
-    .directive('quizAnswer', function () {
+angular.module('bulbs.quiz.questions.question.answer', [
+  // HACK : this is using onion-editor without having a dependency on it, onionEditor directive
+  //    needs to be separated from bulbs-cms then required here
+])
+    .directive('quizEditQuestionsQuestionAnswer', function () {
       return {
-        replace: true,
         restrict: 'E',
-        templateUrl: 'components/edit-pages/quiz/quiz-answer.html',
-        controller: 'QuizEditCtrl',
+        templateUrl: 'bulbs/quiz-edit/quiz-edit-questions/quiz-edit-questions-question/quiz-edit-questions-question-answer/quiz-edit-questions-question-answer.html',
         scope: {
-          'article': '=',
-          'imageGroup': '='
+          answer: '=',
+          quizStyle: '=',
+          outcomes: '='
         }
       };
     });
