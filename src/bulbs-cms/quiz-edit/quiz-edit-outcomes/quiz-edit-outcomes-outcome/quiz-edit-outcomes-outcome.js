9c5055a581ba1c5bdd2dd2d56e58c7f8e13f3b47
Andrew Kos <akos@theonion.com>
2015-09-23 15:27:08 -0500

    Quiz outcome cleanup

--- src/bulbs-cms/quiz-edit/quiz-edit-outcomes/quiz-edit-outcomes-outcome/quiz-edit-outcomes-outcome.js
+++ src/bulbs-cms/quiz-edit/quiz-edit-outcomes/quiz-edit-outcomes-outcome/quiz-edit-outcomes-outcome.js
@@ -1,15 +1,22 @@
 'use strict';
 
-angular.module('bulbs.quiz.edit.outcomes.outcome', [])
-    .directive('quizOutcome', function () {
-      return {
-        replace: true,
-        restrict: 'E',
-        templateUrl: 'components/edit-pages/quiz/quiz-outcome.html',
-        controller: 'QuizEditCtrl',
-        scope: {
-          'article': '=',
-          'imageGroup': '='
+angular.module('bulbs.quiz.edit.outcomes.outcome', [
+  // HACK : import editable from another 3rd party package, not bulbs-cms
+  'bettyEditable'
+])
+  .directive('quizEditOutcomesOutcome', function () {
+    return {
+      controller: [
+        '$scope', 'uuid4',
+        function ($scope, uuid4) {
+          $scope.uuid = uuid4.generate();
         }
-      };
-    });
+      ],
+      restrict: 'E',
+      templateUrl: 'bulbs/quiz-edit/quiz-edit-outcomes/quiz-edit-outcomes-outcome/quiz-edit-outcomes-outcome.html',
+      scope: {
+        outcome: '=',
+        quizStyle: '='
+      }
+    };
+  });
